84ff78c17ef9a18089844dcf0fbe91803aa14b9f
koko <github@koko-campus.com>
2022-09-23 06:54:24 +0000

    テンプレート作成。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate rug;
+use rug::*;
+use ops::Pow;
+use std::fs;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let prec = 32 * 32 * 2048;
+	let one = Float::with_val(prec, 1);
+	let mut small:Float = Float::with_val(prec, 0.1).pow(1000);
+
+    let mut file = fs::File::create("fruits.txt").unwrap();
+    let contents = format!("{}", Float::from(one + small).pow(Integer::from(10).pow(1000)));
+    file.write_all(contents.as_bytes()).unwrap();
 }
