02bc9af5cefd259151ed7ab1c5483a7bdfe61ee6
osawa-koki <github@osawa.dev>
2022-11-12 19:44:16 +0900

    コードスタイル調整。

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use std::fs;
 use std::io::Write;
 
 fn main() {
-    let prec = 32 * 32 * 2048;
-	let one = Float::with_val(prec, 1);
-	let mut small:Float = Float::with_val(prec, 0.1).pow(1000);
+  let prec = 32 * 32 * 2048;
+  let one = Float::with_val(prec, 1);
+  let mut small:Float = Float::with_val(prec, 0.1).pow(1000);
 
-    let mut file = fs::File::create("fruits.txt").unwrap();
-    let contents = format!("{}", Float::from(one + small).pow(Integer::from(10).pow(1000)));
-    file.write_all(contents.as_bytes()).unwrap();
+  let mut file = fs::File::create("fruits.txt").unwrap();
+  let contents = format!("{}", Float::from(one + small).pow(Integer::from(10).pow(1000)));
+  file.write_all(contents.as_bytes()).unwrap();
 }
